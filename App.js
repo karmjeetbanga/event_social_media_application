e3298377276a1d821122b0e6a216db185b78beab
Karmjeet Kaur <karmjeet.k@adorebeauty.com.au>
2022-10-01 09:50:43 +1000

    creating screens for app and add stack navigation functionality to app.jsx

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingScreen from './Screens/LandingScreen';
+import CreateScreen from './Screens/CreateScreen';
+import EventScreen from './Screens/EventScreen';
+
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen name="LandingScreen" component={LandingScreen}/>
+        <Stack.Screen name="CreateScreen" component={CreateScreen}/>
+        <Stack.Screen name="EventScreen" component={EventScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Screens/CreateScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function CreateScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>
+      Create screen here 🎉
+      </Text>
+    </View>
+  );
+}
